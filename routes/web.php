aafd9c869bed435da96ee9d5b664d89ab000a295
Gabriel Cabral <cabral.9santos@gmail.com>
2019-01-22 00:42:15 -0200

    sgcjj - implementando tema do dashboard na página de login e página principal

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/admin', function(){
+    return view('home');
+});
+
+Route::get('/admin/login', ['as' => 'admin.login', function(){
+    return view('admin.login.login');
+}]);
+Route::get('/admin', ['as' => 'admin.principal', function(){
+    return view('admin.principal.index');
+}]);

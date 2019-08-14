a9893ee3918b0cfbe3efbf8b531fb643c83ac3f0
Gabriel Cabral <cabral.9santos@gmail.com>
2019-08-13 21:23:29 -0300

    Migration e Model de Professores

---
+++ database/migrations/2019_08_13_211255_create_teachers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('bad')->nullable();
+            $table->string('document')->unique();
+            $table->string('document_secondary', 20)->nullable();
+            $table->string('cell')->nullable();
+            $table->string('email');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
